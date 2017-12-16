04bea434f2fffcd142a49aca8ac7aae01494e2ca
mkddr <dadang@kibar.co.id>
2017-12-16 10:42:22 +0700

    first

---
+++ app/src/main/java/com/paung/popularmovie/Networking/Endpoint.java
@@ -0,0 +1,16 @@
+package com.paung.popularmovie.Networking;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by mkddr on 16/12/17.
+ */
+
+public interface Endpoint {
+    int VERSION = 3;
+    String API_KEY = "b3162b892dc842005579761440afb2c4";
+
+    @GET(VERSION+"/movie/popular?api_key="+API_KEY+"&language=en-US")
+    Call<ModelMovie> popularMovies();
+}
